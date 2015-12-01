742266f04d11ab8f5938faf1955f2d79dfbb2887
Peter Nagy <peternagyxx@gmail.com>
2015-12-01 14:27:31 +0100

    change building

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm install
-if [ $? -ne 0 ]; then echo "npm install failed"; exit 10; fi
-npm test
-if [ $? -ne 0 ]; then echo "npm test failed"; exit 10; fi
-node_modules/gulp/bin/gulp.js build
-if [ $? -ne 0 ]; then echo "gulp build failed"; exit 10; fi
